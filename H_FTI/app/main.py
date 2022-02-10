1f2591797797bdc14395971f6775c282d65accc6
Pavlo Boiko <pboyko172839465@gmail.com>
2022-02-10 22:31:05 +0200

    Update new model

--- H_FTI/app/main.py
+++ H_FTI/app/main.py
@@ -4,8 +4,6 @@ import cv2
 import tensorflow as tf
 import numpy as np
 
-
-
 from flask import Flask
 from PIL import Image
 import os
@@ -32,7 +30,6 @@ gbr_result = None
 path_best_model = './best_model.h5'
 
 
-
 @app.route('/')
 def first_page():
     return str(data)
@@ -40,7 +37,6 @@ def first_page():
 
 @app.route('/image', methods=['GET', 'POST'])
 def image_read():
-
     if request.method == 'POST':
         if 'file1' not in request.files:
             return 'there is no file1 in form!'
@@ -70,7 +66,6 @@ def api_image():
     return str(predict)
 
 
-
 @app.errorhandler(404)
 def page_not_found(e):
     return "<h1>404</h1><p>The resource could not be found.</p>", 404
@@ -82,14 +77,24 @@ def func(img):
 
     DIM = (width, height)
 
-    # img = cv2.imread("frame347.png")
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    test_image = np.resize(gray, (1, *DIM[::-1]))
+    frame = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-    # imgplot = plt.imshow(gray)
-    # plt.show()
+    frame = cv2.resize(frame, DIM, interpolation=cv2.INTER_AREA) / 255.0
+    frame = np.array([frame])
+    # test_image = np.reshape(frame, (1, *DIM[::-1]))
 
-    predicted = loaded_model.predict(test_image)
+    predicted = (loaded_model_1.predict(frame) + loaded_model_2.predict(frame)) / 2
+    # predicted = predicted.astype(int)
+    #
+    # # img = cv2.imread("frame347.png")
+    # gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    # test_image = np.resize(gray, (1, *DIM[::-1]))
+    #
+    # # imgplot = plt.imshow(gray)
+    # # plt.show()
+    #
+    # predicted = loaded_model.predict(test_image)
+    predicted = predicted.astype(int)
 
     return predicted
 
@@ -102,12 +107,12 @@ if __name__ == "__main__":
 
     path_best_model = './best_model.h5'
 
-    loaded_model = tf.keras.models.load_model(path_best_model)
+    # loaded_model = tf.keras.models.load_model(path_best_model)
 
+    loaded_model_1 = tf.keras.models.load_model(path_best_model)
+    loaded_model_2 = tf.keras.models.load_model(path_best_model)
 
     log = logg.setup_logging('Server')
     log = logg.get_log("Web-server")
 
     app.run(debug=False, host='0.0.0.0', port=3067)
-
-
