56a8fcea9483df36d5a4e30646adf7346751831f
Pavlo Boiko <pboyko172839465@gmail.com>
2022-02-11 02:37:41 +0200

    Update html and api respon

--- H_FTI/app/main.py
+++ H_FTI/app/main.py
@@ -1,14 +1,14 @@
-# This is a  Python script.
-
+import os
+import logg
+import ssl
 import cv2
 import tensorflow as tf
 import numpy as np
 
-from flask import Flask
+from flask import Flask, render_template, request
 from PIL import Image
-import os
-import logg
-import ssl
+
+from Result import Result
 
 try:
     _create_unverified_https_context = ssl._create_unverified_context
@@ -17,25 +17,13 @@ except AttributeError:
 else:
     ssl._create_default_https_context = _create_unverified_https_context
 
-from flask import request, jsonify
 
-UPLOAD_FOLDER = './'
 log = None
 app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-
-data = "<h1>Кусь</h1>"
-prediction = None
-gbr_result = None
 path_best_model = './best_model.h5'
 
 
-@app.route('/')
-def first_page():
-    return str(data)
-
-
-@app.route('/image', methods=['GET', 'POST'])
+@app.route('/', methods=['GET', 'POST'])
 def image_read():
     if request.method == 'POST':
         if 'file1' not in request.files:
@@ -43,16 +31,19 @@ def image_read():
 
         img = Image.open(request.files['file1'])
         img = np.array(img)
-        predict = func(img)
-        return str(predict)
+        predict = predict_result(img)
+        iter = 0
+        results = []
+        for result in list(predict[0]):
+            if iter == 0:
+                results.append(Result(50, result, 250))
+            else:
+                results.append(Result(iter, result))
+            iter = iter + 250
 
-    return '''
-      <h1>Upload new File</h1>
-      <form method="post" enctype="multipart/form-data">
-        <input type="file" name="file1">
-        <input type="submit">
-      </form>
-      '''
+        return render_template("tables.html", results=results)
+
+    return render_template("image.html")
 
 
 @app.route('/api/v1/resources/image', methods=['POST'])
@@ -62,8 +53,29 @@ def api_image():
 
     img = Image.open(request.files['file1'])
     img = np.array(img)
-    predict = func(img)
-    return str(predict)
+    predict = predict_result(img)
+
+    results = []
+    iter = 0
+    for result in list(predict[0]):
+
+        if iter == 0:
+            results.append(
+                {
+                    'range': '50-250',
+                    'result': int(result)
+                }
+            )
+        else:
+            results.append(
+                {
+                    'range': f'{iter}-{iter + 250}',
+                    'result': int(result)
+                }
+            )
+        iter = iter + 250
+
+    return {'data': results}
 
 
 @app.errorhandler(404)
@@ -71,29 +83,17 @@ def page_not_found(e):
     return "<h1>404</h1><p>The resource could not be found.</p>", 404
 
 
-def func(img):
+def predict_result(img):
     width = 720
     height = 576
 
     DIM = (width, height)
 
     frame = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
     frame = cv2.resize(frame, DIM, interpolation=cv2.INTER_AREA) / 255.0
     frame = np.array([frame])
-    # test_image = np.reshape(frame, (1, *DIM[::-1]))
 
     predicted = (loaded_model_1.predict(frame) + loaded_model_2.predict(frame)) / 2
-    # predicted = predicted.astype(int)
-    #
-    # # img = cv2.imread("frame347.png")
-    # gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    # test_image = np.resize(gray, (1, *DIM[::-1]))
-    #
-    # # imgplot = plt.imshow(gray)
-    # # plt.show()
-    #
-    # predicted = loaded_model.predict(test_image)
     predicted = predicted.astype(int)
 
     return predicted
@@ -106,8 +106,6 @@ if __name__ == "__main__":
         os.makedirs(log_directory)
 
     path_best_model = './best_model.h5'
-
-    # loaded_model = tf.keras.models.load_model(path_best_model)
 
     loaded_model_1 = tf.keras.models.load_model(path_best_model)
     loaded_model_2 = tf.keras.models.load_model(path_best_model)
